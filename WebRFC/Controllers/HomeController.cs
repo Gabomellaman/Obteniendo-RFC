0b908168fc5249c35ea7222cd7c1c0594b41eef7
gabomellamann <gabomellamann@gmail.com>
2021-07-19 00:02:15 -0500

    Add project files.

---
+++ WebRFC/Controllers/HomeController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebRFC.Models;
+
+namespace WebRFC.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        BusRFC obj = new BusRFC();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BaseDatos()
+        {
+            try
+            {
+                using (G10BEntities db = new G10BEntities())
+                {
+                    List<UsuariosRFC> ls = db.UsuariosRFC.ToList();
+                    UsuariosRFC repetido = new UsuariosRFC();                   
+                    //ls = db.UsuariosRFC.SqlQuery("Select Id, Nombre,Paterno, Materno, RFC, FNacimiento, Row_Number() over(partition by  rfc order by rfc desc) as Repetidos from UsuariosRFC").ToList();
+                    TempData["contar"] = " Existen "+ db.UsuariosRFC.Count() + " registros en la base de datos";
+                   return View(ls.OrderBy(x=> x.Nombre));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["error"] = ex.Message;
+                return View();
+            }
+        }
+        public ActionResult IngresaDatos()
+        {
+            return  View();
+        }
+        [HttpPost]
+        public ActionResult IngresaDatos(UsuariosRFC u)
+        {
+            try
+            {
+                
+                obj.ValidarRFC(u);
+                using (G10BEntities db = new G10BEntities())
+                {
+                    
+                    string dd = u.FNacimiento.ToString("yyMMdd");
+                    string rfc = obj.CrearRFC(u);
+                    
+                    bool evitar= Convert.ToBoolean( db.PalabrasExcentas.Where(x => x.Nombre.Contains(rfc)).Select(x=> 1).FirstOrDefault());
+                    if (evitar == true)
+                    {                        
+                        string afrc = rfc.Substring(0,3) + 'X'+ dd;
+                        u.RFC = afrc;
+                    }
+                    else
+                    {
+                        rfc += dd;
+                        u.RFC = rfc;
+                    }
+
+                    db.UsuariosRFC.Add(u);
+
+                    db.SaveChanges();
+                    return View("RFC", u);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+                TempData["error"] = ex.Message;
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (G10BEntities db= new G10BEntities())
+                {
+                    UsuariosRFC us = db.UsuariosRFC.Where(x => x.Id == id).FirstOrDefault();
+                    return View(us);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["error"]= ex.Message;
+                return RedirectToAction("BaseDatos");
+            }
+            
+        }
+        [HttpPost]
+        public ActionResult Delete(UsuariosRFC us)
+        {
+            try
+            {
+                using (G10BEntities db = new G10BEntities())
+                {
+                    UsuariosRFC borrar = db.UsuariosRFC.Find(us.Id);
+                    db.UsuariosRFC.Remove(borrar);
+                    db.SaveChanges();
+                    return RedirectToAction("BaseDatos");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["error"] = ex.Message;
+                return View(us);
+            }
+
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (G10BEntities db = new G10BEntities())
+                {
+                    UsuariosRFC us = db.UsuariosRFC.Where(x => x.Id == id).FirstOrDefault();
+                    return View(us);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["error"] = ex.Message;
+                return RedirectToAction("BaseDatos");
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Edit(UsuariosRFC us)
+        {
+            try
+            {
+                using (G10BEntities db = new G10BEntities())
+                {
+                    UsuariosRFC modificar = db.UsuariosRFC.Find(us.Id);
+                    modificar.Nombre = us.Nombre;
+                    modificar.Paterno = us.Paterno;
+                    modificar.Materno = us.Materno;
+                    modificar.FNacimiento = us.FNacimiento;
+
+
+                    string dd = us.FNacimiento.ToString("yyMMdd");
+                    string rfc = obj.CrearRFC(us);
+
+                    bool evitar = Convert.ToBoolean(db.PalabrasExcentas.Where(x => x.Nombre.Contains(rfc)).Select(x => 1).FirstOrDefault());
+                    if (evitar == true)
+                    {
+                        string afrc = rfc.Substring(0, 3) + 'X' + dd;
+                        us.RFC = afrc;
+                    }
+                    else
+                    {
+                        rfc += dd;
+                        us.RFC = rfc;
+                    }
+
+                    modificar.RFC = us.RFC;
+
+                    db.SaveChanges();
+                    return RedirectToAction("BaseDatos");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                TempData["error"] = ex.Message;
+                return View(us);
+            }
+        }
+        public ActionResult Buscar(string Buscar) 
+        {
+            try
+            {
+                using (G10BEntities db = new G10BEntities())
+                {
+                    List<UsuariosRFC> us = db.UsuariosRFC.Where(x => x.Nombre.Contains(Buscar) || x.Paterno.Contains(Buscar)|| x.Materno.Contains(Buscar)|| x.FNacimiento.ToString().Contains(Buscar) || x.RFC.Contains(Buscar)).ToList();
+                    return View("BaseDatos", us);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return RedirectToAction("BaseDatos");
+            }
+        }
+    }
+}
---
+++ WebRFC/Models/BusRFC.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRFC.Models
+{
+    public class BusRFC
+    {
+        public void ValidarRFC(UsuariosRFC a)
+        {
+            
+            if (a.Nombre.Trim()[0] =='.')
+            {
+               throw new Exception("Nombre o apellidos no pueden empezar con un '.'");
+            }
+            if (a.Paterno.Trim()[0] == '.')
+            {
+                throw new Exception("Nombre o apellidos no pueden empezar con un '.'");
+            }
+            if (a.Materno!=null)
+            {
+                if (a.Materno.Trim()[0] == '.')
+                {
+                throw new Exception("Nombre o apellidos no pueden empezar con un '.'");
+                }
+                
+            }
+        }
+        public string CrearRFC(UsuariosRFC a)
+        {
+            string n = a.Nombre.Trim().ToUpper();
+            string p = a.Paterno.Trim().ToUpper();
+            
+            string rfc = "";
+            //filtro apellido paterno, empezando si es de un solo caracter
+            if (p.Length == 1)
+            {
+                rfc += p + "X";
+            }
+            else
+            {
+                //Validando si es apellido compuesto
+                string[] paterno = p.Split(' ');
+                
+                    //Validando si el apellido paterno  tiene mas de 2 letras en la primer parte en caso de ser compuesto
+                    if (paterno[0].Length == 1)
+                    {
+                        rfc += paterno[0]+ "X";
+                    }
+                    else
+                    {
+                        //quitar primer caracter del  apellido paterno  y revisar si tiene despues del primer char vocales
+                        rfc =ValidarVocales(paterno[0]);
+                    }   
+            }
+            //----------------Empezamos con apellido materno----------------------------------//
+            
+            if (a.Materno == null)
+            {
+                rfc += "X";
+            }
+            else
+            {
+                string m = a.Materno.Trim().ToUpper();
+                char sustituir = m[0];
+                if (m[0] == 'Ñ')
+                {
+                    sustituir = 'X';
+                }
+                rfc +=sustituir;
+            }
+            //-------------Nombre---------------------------------
+            //Validando si tiene dos nombres
+            string[] nombre = n.Split(' ');
+            
+            //Validando si el segundo nombre empieza con Ñ
+            if (nombre.Length >=2)
+            {
+                string segundoNombre = nombre[1];
+                char sustituir = segundoNombre[0];
+                if (segundoNombre[0] == 'Ñ')
+                {
+                    sustituir = 'X';
+                }
+                rfc += sustituir;
+            }
+            else
+            {
+                string unicoNombre = nombre[0];
+                char sustituir = unicoNombre[0];
+                if (unicoNombre[0] == 'Ñ')
+                {
+                    sustituir = 'X';
+                }
+                rfc += sustituir;
+            }
+            return rfc;
+        }
+        public string Cambiar(string a)
+        {
+            string cambiar = "";
+            for (int i = 0; i < a.Length-1; i++)
+            {
+                cambiar += a[i];
+            }
+            cambiar += 'X';
+            return cambiar;
+        }
+        public string ValidarVocales(string a)
+        {
+            char sustituir = a[0];
+            if (a[0] == 'Ñ')
+            {
+                sustituir = 'X';
+            }
+            string componentesRFC = "";
+            string quit = a.Substring(1, a.Length - 1);
+            for (int i = 0; i < quit.Length; i++)
+            {
+                if (quit[i] == 'A')
+                {
+                    componentesRFC += sustituir + "A";
+                    break;
+
+                }
+                if (quit[i] == 'E')
+                {
+                    componentesRFC += sustituir + "E";
+                    break;
+                }
+                if (quit[i] == 'I')
+                {
+                    componentesRFC += sustituir + "I";
+                    break;
+                }
+                if (quit[i] == 'O')
+                {
+                    componentesRFC += sustituir + "O";
+                    break;
+                }
+                if (quit[i] == 'U')
+                {
+                    componentesRFC += sustituir + "U";
+                    break;
+                }
+                if (i == quit.Length - 1)
+                {
+                    componentesRFC += sustituir + "X";
+                }
+            }
+            return componentesRFC;
+        }
+    }
+}
